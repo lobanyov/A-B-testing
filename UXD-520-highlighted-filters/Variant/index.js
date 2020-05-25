6d7592734f28d7aa8cf33c99e49935d712dd974d
lobanyov <stevenson@tut.by>
2020-05-25 18:13:28 +0300

    feat: add overlay

--- UXD-520-highlighted-filters/Variant/index.js
+++ UXD-520-highlighted-filters/Variant/index.js
@@ -1,7 +1,24 @@
 if (document.readyState !== 'loading') {
-  // Case 1
+  insertOverlay();
 } else {
   window.addEventListener('load', function() {
-    // Case 2
+    insertOverlay();
   });
+}
+
+/**
+ * For the gray overlay need to create div element with z-index eqauls 10.
+ * In the same time, aside needs higher z-index then the div has. For example: 11.
+ * Also, the overlay needs these styles according UXD-520 ticket description:
+ * background: #000;
+ * opacity: 0.5;
+ */
+
+function insertOverlay() {
+  var documentEl = document.documentElement;
+  var overlay = document.createElement('div');
+  overlay.className = 'UXD-520-overlay';
+  overlay.style.height = documentEl.scrollHeight + 'px';
+
+  documentEl.insertAdjacentElement('afterbegin', overlay);
 }
