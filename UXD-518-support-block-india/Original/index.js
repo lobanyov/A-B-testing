fb6b536d62d8ac95b5f6eeadaf76a6fbce674bd8
lobanyov <stevenson@tut.by>
2020-05-28 17:20:56 +0300

    add click-events to tips block for original

--- UXD-518-support-block-india/Original/index.js
+++ UXD-518-support-block-india/Original/index.js
@@ -5,10 +5,21 @@ function addClassesToSupportLinks() {
   });
 }
 
+function addClassesToTips() {
+  var tipsSection = document.querySelector('[data-tpl-name="Content - Flexible Blocks"]');
+  var tips = tipsSection.querySelectorAll('[class^="sdl-content-flexible-blocks_column se-col-md-"]');
+
+  tips.forEach(function(el) {
+    el.classList.add('UXD-508-support-item-tip');
+  });
+}
+
 if (document.readyState !== 'loading') {
   addClassesToSupportLinks();
+  addClassesToTips();
 } else {
   window.addEventListener('load', function() {
     addClassesToSupportLinks();
+    addClassesToTips();
   });
 }
