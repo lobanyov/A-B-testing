beb36e69340b032324f840eef517e247eeb85999
lobanyov <stevenson@tut.by>
2020-06-26 16:23:47 +0300

    [UXD-525]: fix index.js for Original

--- UXD-525-danish-homepage/Original/index.js
+++ UXD-525-danish-homepage/Original/index.js
@@ -1,16 +1,28 @@
 var config = {
   selectors: {
     descriptionLinkSelector: '.sdl-heading-section-title-h3 .se-font-text2-0 > .se-col > a',
+    smallSquareImageSelector: '.sdl-content-small-square-image',
+    rectangularImageSelector: '.sdl-content-rectangular-image',
   },
 
   getNodes: function() {
     this.nodes = {
       descriptionLink: document.querySelector(this.selectors.descriptionLinkSelector),
+      smallSquareImages: document.querySelectorAll(this.selectors.smallSquareImageSelector),
+      rectangularImages: document.querySelectorAll(this.selectors.rectangularImageSelector),
     };
   },
 
   applyChanges: function() {
     this.nodes.descriptionLink.classList.add('uxd-525-primary');
+
+    this.nodes.smallSquareImages.forEach(function(img) {
+      img.classList.add('uxd-525-secondary');
+    });
+
+    this.nodes.rectangularImages.forEach(function(img) {
+      img.classList.add('uxd-525-secondary');
+    });
   },
 
   init: function() {
