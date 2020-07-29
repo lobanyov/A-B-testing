aa244e2e56b6a5cab593b02782b99d123e38b618
lobanyov <stevenson@tut.by>
2020-07-29 13:36:15 +0300

    UXD-441: remove redundant code from Original

--- UXD-441-tab-vs-filters/Original/index.js
+++ UXD-441-tab-vs-filters/Original/index.js
@@ -1,10 +1,6 @@
 var UXD441 = {
   config: {
-    pages: {
-      first: '1',
-      second: '2',
-    },
-    observerConfig: {
+    observer: {
       childList: true,
       subtree: true,
     }
@@ -15,29 +11,24 @@ var UXD441 = {
     targetPageLinksSelector: '.pager a',
     filterItemsSelector: 'aside .filter-lvl2.subcategory-item',
     resultCardsSelector: '.results-list-holder .single-result',
+    resultsListSelector: '.se-search-layout-results',
     primaryMetricSelecor: '.experimentUXD_DE428',
     secondaryMetricSelector: '.experimentUXD_DE428_card-clicks',
-    filtersSelector: '.categories-body',
-    paginationSelector: '.pagination-bar',
   },
 
   getNodes: function() {
     this.nodes = {
       targetPageLink: document.querySelector(this.selectors.targetPageLinkSelector),
       targetPageLinks: document.querySelectorAll(this.selectors.targetPageLinksSelector),
-      filterItems: document.querySelectorAll(this.selectors.filterItemsSelector),
-      resultCards: document.querySelectorAll(this.selectors.resultCardsSelector),
-      filters: document.querySelector(this.selectors.filtersSelector),
-      pagination: document.querySelector(this.selectors.paginationSelector),
+      resultsList: document.querySelector(this.selectors.resultsListSelector),
     };
   },
 
-  validatePage: function() {
-    return this.nodes.targetPageLink.dataset.page === this.config.pages.first ||
-      this.nodes.targetPageLink.dataset.page === this.config.pages.second;
-  },
-
   addClassesForElements: function() {
+    // filters & result cards are changed dynamically
+    this.nodes.filterItems = document.querySelectorAll(this.selectors.filterItemsSelector);
+    this.nodes.resultCards = document.querySelectorAll(this.selectors.resultCardsSelector);
+
     this.nodes.filterItems.forEach(function(el) {
       el.classList.add('experimentUXD_DE428');
     });
@@ -47,67 +38,25 @@ var UXD441 = {
     });
   },
 
-  removeClassesFromElements: function() {
-    this.nodes.primaryMetricNodes = document.querySelectorAll(primaryMetricSelecor);
-    this.nodes.secondaryMetricNodes = document.querySelectorAll(secondaryMetricSelector);
-
-    this.nodes.primaryMetricNodes.forEach(function(el) {
-      el.classList.remove('experimentUXD_DE428');
-    });
-
-    this.nodes.secondaryMetricNodes.forEach(function(el) {
-      el.classList.remove('experimentUXD_DE428_card-clicks');
-    });
-  },
-
-  applyChangesForValidPages: function() {
-    var _this = this;
-
-    this.nodes.targetPageLinks.forEach(function(el) {
-      if (el.dataset.page > _this.config.pages.second) {
-        el.addEventListener('click', _this.removeClassesFromElements);
-      } else {
-        el.addEventListener('click', _this.addClassesForElements);
-      }
-    });
-  },
-
   initObserver: function() {
-    var _this = this;
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function (mutation) {
-        if (_this.validatePage()) {
-          _this.addClassesForElements();
-        } else {
-          _this.removeClassesFromElements();
-        }
+    var obsCallback = this.addClassesForElements.bind(this);
+    var observer = new MutationObserver(obsCallback);
 
-        _this.applyChangesForValidPages();
-      });
-
-      observer.observe(filters, this.config.observerConfig);
-      observer.observe(pagination, this.config.observerConfig);
-    });
+    observer.observe(this.nodes.resultsList, this.config.observer);
   },
 
   init: function() {
     if (document.readyState !== 'loading') {
       this.getNodes();
+      this.addClassesForElements();
       this.initObserver();
-      if (this.validatePage()) {
-        this.addClassesForElements();
-      }
-      this.applyChangesForValidPages();
     } else {
       var _this = this;
 
       window.addEventListener('load', function() {
         _this.getNodes();
+        _this.addClassesForElements();
         _this.initObserver();
-        if (_this.validatePage()) {
-          _this.addClassesForElements();
-        }
-        _this.applyChangesForValidPages();
       });
     }
   },
