1c1bf3b5f258d1948291e2a73552e6304d8b0bd5
lobanyov <stevenson@tut.by>
2020-08-14 11:50:08 +0300

    UXD-547: update desktop Variation

--- UXD-547-old-new-rego-pages/Variant/index.js
+++ UXD-547-old-new-rego-pages/Variant/index.js
@@ -2,12 +2,18 @@ var UXD547 = {
   selectors: {
     emailFieldWrapperSelector: '.form-group.padd-bt-7',
     registerTextSelector: '.text-center',
+    registerLinkSelector: '.text-center > a',
+    mySchneiderBlockSelector: '.td-cell',
+    privacyNoticeSelector: '.privacy-notice',
   },
 
   getNodes: function() {
     this.nodes = {
       emailFieldWrapper: document.querySelector(this.selectors.emailFieldWrapperSelector),
       registerText: document.querySelector(this.selectors.registerTextSelector),
+      registerLink: document.querySelector(this.selectors.registerLinkSelector),
+      mySchneiderBlock: document.querySelector(this.selectors.mySchneiderBlockSelector),
+      privacyNotice: document.querySelector(this.selectors.privacyNoticeSelector),
     };
   },
 
@@ -16,7 +22,27 @@ var UXD547 = {
     p.textContent = 'Enter your email address to get started';
     p.className = 'UXD-547-paragraph';
     this.nodes.emailFieldWrapper.insertAdjacentElement('beforebegin', p);
-    this.nodes.registerText.remove();
+    this.nodes.registerText.style.display = 'none';
+
+    var registrationBtn = document.createElement('button');
+    registrationBtn.className = 'UXD-547-registration-btn';
+    registrationBtn.textContent = 'New to Schneider Electric? Create an account';
+    registrationBtn.href = '#';
+    this.nodes.mySchneiderBlock.insertAdjacentElement('afterend', registrationBtn);
+
+    var helpLink = document.createElement('a');
+    helpLink.className = 'UXD-547-help-link';
+    helpLink.textContent = 'Need Help?';
+    helpLink.href = '#';
+    this.nodes.privacyNotice.insertAdjacentElement('afterend', helpLink);
+
+    // Add listener to registration button
+    var cb = this.registrationBtnHandler.bind(this);
+    registrationBtn.addEventListener('click', cb);
+  },
+
+  registrationBtnHandler: function() {
+    this.nodes.registerLink.click();
   },
 
   init: function() {
