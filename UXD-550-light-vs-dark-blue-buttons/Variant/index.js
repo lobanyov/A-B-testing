a575ef101ff9bced4c135abfe471347f530189ca
lobanyov <stevenson@tut.by>
2020-10-20 14:54:51 +0300

    UXD-550: fix link font size

--- UXD-550-light-vs-dark-blue-buttons/Variant/index.js
+++ UXD-550-light-vs-dark-blue-buttons/Variant/index.js
@@ -13,16 +13,29 @@ var UXD550 = {
   },
 
   applyChanges: function() {
+    function setInheritSize(link) {
+      var span = link.querySelector('span');
+      if (span) {
+        span.style.fontSize = 'inherit';
+        span.style.lineHeight = 'inherit';
+      }
+    }
+
     this.nodes.buttons.forEach(function(btn) {
       if (btn.className.indexOf(this.selectors.primary) > -1) {
         btn.style.backgroundColor = '#0087CD';
         btn.style.fontSize = '14pt';
         btn.style.fontWeight = '700';
 
+        if (btn.tagName === 'A') setInheritSize(btn);
+
         btn.classList.add('uxd-550-primary');
       } else if (btn.className.indexOf(this.selectors.secondary) > -1) {
         btn.style.fontSize = '14pt';
         btn.style.fontWeight = '700';
+        btn.style.outline = '1px solid #0087CD';
+
+        if (btn.tagName === 'A') setInheritSize(btn);
 
         btn.classList.add('uxd-550-secondary');
       }
